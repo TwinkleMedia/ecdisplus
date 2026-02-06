8e6a6d763c6b296cb6232eafd2b66463b0dca690
twinklekaur13 <twinklerajan1206@gmail.com>
2026-02-06 10:57:03 +0530

    seo

---
+++ courses/404.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Page Not Found | ECDISPLUS Training Solutions</title>
+</head>
+<body>
+  <h1>Looking for an ECDIS Course Near You?</h1>
+  <p>
+    The page you are looking for has moved.
+    ECDISPLUS offers <strong>ECDIS training in Navi Mumbai & across India</strong>.
+  </p>
+  <a href="/bestecdistrainingcourses.php">View All ECDIS Training Courses</a><br><br>
+  <a href="/jrc-ecdis-course-in-navi-mumbai">JRC ECDIS Course in Navi Mumbai</a>
+</body>
+</html>
