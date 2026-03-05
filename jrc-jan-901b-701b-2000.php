2a3389c329fe670651b100b46f1497580a5b52e4
twinklekaur13 <twinklerajan1206@gmail.com>
2026-03-05 18:28:35 +0530

    update

--- jrc-jan-901b-701b-2000.php
+++ jrc-jan-901b-701b-2000.php
@@ -1,3 +1,29 @@
 <?php
 $courseSlug = 'JRC-JAN-901B/701B';
+// Manually define SEO for this course
+$seo = [
+  'title' => 'JRC JAN-901B / JAN-701B ECDIS Course in Belapur | ECDIS Training India',
+  
+  'description' => 'Join the JRC JAN-901B / JAN-701B ECDIS training course in Belapur, Navi Mumbai. DG approved ECDIS simulator training for deck officers and seafarers. Enroll in JRC ECDIS course near you in India.',
+  
+  'keywords' => 'JRC ECDIS course, JRC JAN 901B training, JRC JAN 701B ECDIS course, JRC ECDIS near me, JRC ECDIS Belapur, ECDIS course Navi Mumbai, JRC ECDIS training India, ECDIS simulator training, DG approved ECDIS course, ECDISPLUS Training Solutions',
+  
+  'canonical' => 'https://ecdisplus.com/jrc-jan-901b-701b.php',
+  
+  'og_title' => 'JRC JAN-901B / JAN-701B ECDIS Course | Belapur Navi Mumbai',
+  
+  'og_desc' => 'Get certified with the JRC JAN-901B / JAN-701B ECDIS training course in Belapur. Professional ECDIS simulator training for seafarers in India.',
+  
+  'og_image' => 'https://ecdisplus.com/assets/new-folder/jrc-ecdis-course.png',
+  
+  'schema_name' => 'JRC JAN-901B / JAN-701B ECDIS Course',
+  
+  'schema_desc' => 'Professional JRC JAN-901B / JAN-701B ECDIS simulator training course for deck officers and maritime professionals in Belapur, Navi Mumbai, India.'
+];
+
+
+
+
+
+
 require './course-template.php';
