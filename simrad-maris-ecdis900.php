c5bb14275910d23fc5ddeb4f53c0c2ea3277c188
twinklekaur13 <twinklerajan1206@gmail.com>
2026-04-18 11:45:43 +0530

    update

--- simrad-maris-ecdis900.php
+++ simrad-maris-ecdis900.php
@@ -1,3 +1,23 @@
 <?php
 $courseSlug = 'SIMRAD-Maris-ECDIS90';
+
+$seo = [
+  'description' => 'Join the best Simrad ECDIS 900 course in Navi Mumbai, Maharashtra with top-class simulator training and expert faculty. DG-approved guidance with Pan India admissions. Search Simrad ECDIS near me and start your maritime career today.',
+  
+  'keywords' => 'Simrad ECDIS 900 course, Simrad ECDIS 900 training, Simrad ECDIS near me, ECDIS course Navi Mumbai, ECDIS course Mumbai, maritime institute India ECDIS, DG approved ECDIS course India, best ECDIS institute Navi Mumbai, ECDIS training Maharashtra, Simrad training India, ECDIS certification Mumbai, ECDIS course near me India',
+  
+  'canonical' => 'https://ecdisplus.com/simrad-ecdis-900-course-navi-mumbai-india.php',
+  
+  'og_title' => 'Simrad ECDIS 900 Course in Navi Mumbai | Best ECDIS Training Institute India',
+  
+  'og_desc' => 'Enroll in Simrad ECDIS 900 training in Navi Mumbai with hands-on simulator practice, expert trainers, and Pan India admissions. Your top choice for ECDIS course near you.',
+  
+  'og_image' => 'https://ecdisplus.com/assets/new-folder/simrad-ecdis-900.png',
+  
+  'schema_name' => 'Simrad ECDIS 900 Course',
+  
+  'schema_desc' => 'Professional Simrad ECDIS 900 training course in Navi Mumbai, Maharashtra with simulator-based learning and Pan India enrollment for maritime professionals.'
+];
+
 require './course-template.php';
+?>
